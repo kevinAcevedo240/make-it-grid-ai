423178ff081a886459f1467a895e1494b4b6be2f
Kevin Julian Acevedo Salazar <kevinAcevedo240@users.noreply.github.com>
2024-06-19 23:17:15 -0500

    Update: Center Id item

--- components/React-grid-layout.tsx
+++ components/React-grid-layout.tsx
@@ -148,7 +148,7 @@ const ExampleGrid = () => {
             {layout.map((item) => (
               <div
                 key={item.i}
-                className="relative text-white bg-primary border border-white rounded-lg p-3 shadow-lg z-10"
+                className="relative text-white bg-primary border flex justify-center items-center border-white rounded-lg p-3 shadow-lg z-10"
               >
                 <button
                   className="absolute top-0 right-0 m-1 p-1 bg-muted/80 text-white rounded-full size-6 flex items-center justify-center cursor-pointer z-20"
