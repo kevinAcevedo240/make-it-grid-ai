3af6eebf6cb078a4b37447d0e4cef012cf9b0390
Kevin Julian Acevedo Salazar <kevinAcevedo240@users.noreply.github.com>
2024-06-20 17:41:24 -0500

    Add: JSX and HTML/CSS  Code and Copy Code method

--- components/GenerateCodeCard.tsx
+++ components/GenerateCodeCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CopyIcon } from "@radix-ui/react-icons";
 import { Button } from './ui/button';
 import {
@@ -8,8 +8,9 @@ import {
     CardTitle,
 } from "@/components/ui/card";
 import { Label } from './ui/label';
-import { generateTailwindCode } from '@/utils/GenerateTailwindCode';
+import { generateCssCode, generateHtmlCode, generateTailwindCode } from '@/utils/GenerateTailwindCode';
 import toast from 'react-hot-toast';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs';
 
 interface GeneratedCodeCardProps {
     rows: number;
@@ -20,9 +21,21 @@ interface GeneratedCodeCardProps {
 }
 
 const GeneratedCodeCard: React.FC<GeneratedCodeCardProps> = ({ rows, cols, gap, layout, codeContainerRef }) => {
+    const [activeTab, setActiveTab] = useState('jsx');
+
     const handleCopyToClipboard = () => {
-        if (codeContainerRef.current) {
-            const codeToCopy = codeContainerRef.current.innerText.trim();
+        let codeToCopy = '';
+        if (activeTab === 'jsx') {
+            if (codeContainerRef.current) {
+                codeToCopy = codeContainerRef.current.innerText.trim();
+            }
+        } else if (activeTab === 'html') {
+            const htmlCode = generateHtmlCode(rows, cols, gap, layout);
+            const cssCode = generateCssCode(rows, cols, gap, layout);
+            codeToCopy = `<! -- HTML CODE -->\n\n${htmlCode}\n\n<! -- CSS CODE -->\n\n${cssCode}`;
+        }
+
+        if (codeToCopy) {
             navigator.clipboard.writeText(codeToCopy)
                 .then(() => toast.success('Code Copied to Clipboard!'))
                 .catch((err) => console.error('Failed to copy:', err));
@@ -30,34 +43,57 @@ const GeneratedCodeCard: React.FC<GeneratedCodeCardProps> = ({ rows, cols, gap, 
     };
 
     return (
-        <Card className="mt-12">
-            <CardHeader>
-                <CardTitle>
-                    <div className="flex justify-between items-center">
-                        <h2 className="text-xl sm:text-2xl">Generated Tailwind Code</h2>
-                        <Button
-                            onClick={handleCopyToClipboard}
-                            className="p-2 dark:border dark:border-primary dark:bg-primary/0"
-                        >
-                            <CopyIcon className="size-4" />
-                        </Button>
-                    </div>
-                </CardTitle>
-            </CardHeader>
-            <CardContent>
-                <div className="grid gap-6">
-                    <div className="grid gap-3">
-                        <Label htmlFor="name">JSX Code:</Label>
-                        <pre
-                            ref={codeContainerRef}
-                            className="border rounded-lg p-2 overflow-x-auto"
-                        >
-                            {generateTailwindCode(rows, cols, gap, layout)}
-                        </pre>
-                    </div>
-                </div>
-            </CardContent>
-        </Card>
+      <Card className="mt-12">
+        <CardHeader>
+          <CardTitle>
+            <div className="flex justify-between items-center">
+              <h2 className="text-xl sm:text-2xl">Generated Tailwind Code</h2>
+              <Button
+                onClick={handleCopyToClipboard}
+                className="p-2 dark:border dark:border-primary dark:bg-primary/0"
+              >
+                <CopyIcon className="size-4" />
+              </Button>
+            </div>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-6">
+            <div className="grid gap-3">
+              <Tabs defaultValue="jsx" onValueChange={setActiveTab} className="overflow-x-auto">
+                <TabsList>
+                  <TabsTrigger value="jsx">JSX</TabsTrigger>
+                  <TabsTrigger value="html">HTML / CSS</TabsTrigger>
+                </TabsList>
+                <TabsContent value="jsx">
+                  <pre
+                    ref={codeContainerRef}
+                    className="border rounded-lg p-2 overflow-x-auto max-h-64 overflow-y-auto"
+                  >
+                    {generateTailwindCode(rows, cols, gap, layout)}
+                  </pre>
+                </TabsContent>
+                <TabsContent value="html">
+                  <div className="md:grid md:grid-cols-2 md:gap-4 space-y-4 md:space-y-0">
+                    <pre
+                      ref={codeContainerRef}
+                      className="border rounded-lg p-2 overflow-x-auto max-h-64 overflow-y-auto"
+                    >
+                      {generateHtmlCode(rows, cols, gap, layout)}
+                    </pre>
+                    <pre
+                      ref={codeContainerRef}
+                      className="border rounded-lg p-2 overflow-x-auto max-h-64 overflow-y-auto"
+                    >
+                      {generateCssCode(rows, cols, gap, layout)}
+                    </pre>
+                  </div>
+                </TabsContent>
+              </Tabs>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
     );
 };
 
--- utils/GenerateTailwindCode.ts
+++ utils/GenerateTailwindCode.ts
@@ -17,3 +17,56 @@ export const generateTailwindCode = (rows: number, cols: number, gap: number, la
   </div>
   `;
 };
+
+export const generateHtmlCode = (rows: number, cols: number, gap: number, layout: Layout[]): string => {
+  const itemPositions = layout.map(item => {
+    const colStart = item.x + 1;
+    const rowStart = item.y + 1;
+    const colSpan = item.w > 1 ? ` col-span-${item.w}` : '';
+    const rowSpan = item.h > 1 ? ` row-span-${item.h}` : '';
+
+    return `
+    <div class="col-start-${colStart} row-start-${rowStart}${colSpan}${rowSpan}">${item.i}</div>`;
+  }).join('');
+
+  return `
+  <div class="grid grid-cols-${cols} grid-rows-${rows} gap-${gap}">
+    ${itemPositions}
+  </div>
+  `;
+};
+
+export const generateCssCode = (rows: number, cols: number, gap: number, layout: Layout[]): string => {
+  const css = `
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(${cols}, 1fr);
+    grid-template-rows: repeat(${rows}, 1fr);
+    gap: ${gap}rem;
+  }
+  `;
+
+  const itemPositions = layout.map(item => {
+    const colStart = item.x + 1;
+    const rowStart = item.y + 1;
+    const colSpan = item.w > 1 ? ` span ${item.w}` : ' span 1';
+    const rowSpan = item.h > 1 ? ` span ${item.h}` : ' span 1';
+
+    return `
+    .col-start-${colStart} {
+      grid-column-start: ${colStart};
+    }
+    .row-start-${rowStart} {
+      grid-row-start: ${rowStart};
+    }
+    .col-span-${item.w} {
+      grid-column: ${colStart} / ${colStart + item.w};
+    }
+    .row-span-${item.h} {
+      grid-row: ${rowStart} / ${rowStart + item.h};
+    }
+    `;
+  }).join('');
+
+  return css + itemPositions;
+};
