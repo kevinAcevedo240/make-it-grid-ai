63978db29ea867e0d66b4c6d696ea519427ba8b3
Kevin Julian Acevedo Salazar <kevinAcevedo240@users.noreply.github.com>
2024-06-22 16:06:42 -0500

    Update: Bigger text title

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,7 @@ import { ModeToggle } from './ui/theme-toggle';
 const NavBar = () => {
   return (
     <div className="flex justify-between">
-      <h1 className="text-2xl font-bold mb-4">MakeItGrid AI</h1>
+      <h1 className="sm:text-4xl text-3xl font-bold mb-4">MakeItGrid AI</h1>
       <ModeToggle />
     </div>
   );
