f76761bf4ec17f3b2fb7c507c2a5ae0f47af8961
Kevin Julian Acevedo Salazar <kevinAcevedo240@users.noreply.github.com>
2024-07-01 00:15:13 -0500

    Feat: Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,72 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "MakeItGrid | Bento Grid Generator",
-  description: "Empower your creativity with MakeItGrid, the ultimate platform for crafting and sharing customizable bento grids. Design, collaborate, and monetize your grid creations effortlessly, powered by AI.",
+  description:
+    "Empower your creativity with MakeItGrid, the ultimate platform for crafting and sharing customizable bento grids. Design, collaborate, and monetize your grid creations effortlessly, powered by AI.",
+  authors: {
+    name: "MakeItGrid Team",
+  },
+  keywords: [
+    "Bento Grid Generator",
+    "Bento Grid Maker",
+    "Bento Grid Creator",
+    "Bento Grid Design",
+    "Bento Grid Tool",
+    "Bento Grid Builder",
+    "Free Bento Grid Generator",
+    "HTML and CSS Grid Generator",
+    "Tailwind CSS Generator",
+    "Tailwind Grid Generator",
+    "Tailwind CSS Grid Generator",
+    "Tailwind CSS Grid Maker",
+    "Tailwind Bento Grid Generator",
+    "CSS Grid Generator",
+    "Responsive Design",
+    "Customizable Grids",
+    "Grid Layouts",
+    "AI Grid Design",
+    "Dark Mode",
+    "Light Mode",
+    "Online Grid Maker",
+    "Online Grid Generator",
+    "Online Grid Builder",
+    "Free Online Grid Builder",
+    "Free Online Grid Generator",
+    "Free Online Bento Grid",
+    "Free Online Bento Grid Generator",
+    "Grid Layout Tool",
+    "Design Platform",
+    "Code Sandbox Integration",
+    "Web Design",
+    "Front-end Development",
+    "React",
+    "TypeScript",
+    "Shadcn UI"
+  ],
+  robots: "index, follow",
+  icons: {
+    icon: {
+      url: "/favicon.ico",
+      type: "image/x-icon",
+    },
+    shortcut: { url: "/favicon.ico", type: "image/x-icon" },
+  },
+  openGraph: {
+    title: "MakeItGrid | Bento Grid Generator",
+    description:
+      "Empower your creativity with MakeItGrid, the ultimate platform for crafting customizable bento grids.",
+    url: "https://make-it-grid-ai.vercel.app/",
+    type: "website",
+    images: [
+      {
+        url: "https://make-it-grid-ai.vercel.app/og-image.png", 
+        width: 800,
+        height: 600,
+        alt: "MakeItGrid Logo",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
