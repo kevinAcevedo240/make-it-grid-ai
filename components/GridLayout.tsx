a12323908bbc68f0fdaf731334cdb078afcc02ff
Kevin Julian Acevedo Salazar <kevinAcevedo240@users.noreply.github.com>
2024-06-30 23:49:58 -0500

    Refactor: minimize icon add when it has more than 8 columns and is a mobile version

--- components/GridLayout.tsx
+++ components/GridLayout.tsx
@@ -88,7 +88,7 @@ const GridLayout = () => {
                       className="relative border border-muted-foreground/30 dark:border-gray-100 transition-all duration-300 hover:bg-secondary/30 rounded-lg h-full flex items-center justify-center z-0 cursor-pointer"
                       style={{ zIndex: 1 }}
                     >
-                      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-primary text-white rounded-full size-6 sm:size-8 flex items-center justify-center  z-1">
+                      <div className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-primary text-white rounded-full ${(isMobile || !isDesktopOrLaptop) && cols >= 8 ? "size-5" : "size-6 sm:size-8"} flex items-center justify-center  z-1`}>
                         <span className="text-xl sm:text-2xl">+</span>
                       </div>
                     </div>
