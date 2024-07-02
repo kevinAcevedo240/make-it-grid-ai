5a405b8557adc54834b2ddf254f3bc1a122c8277
Kevin Julian Acevedo Salazar <kevinAcevedo240@users.noreply.github.com>
2024-07-02 18:33:22 -0500

    Update: Website and Github handleClick

--- components/menu/github.tsx
+++ components/menu/github.tsx
@@ -9,17 +9,25 @@ import {
 } from "@/components/ui/tooltip";
 
 export const GitHub = () => {
+  const handleClick = () => {
+    window.open("https://github.com/kevinAcevedo240/make-it-grid-ai", "_blank");
+  };
+
   return (
     <Tooltip>
       <TooltipTrigger asChild>
-        <Button className="github-step dark:border-primary bg-muted-foreground dark:border dark:bg-transparent active:scale-90">
-          <Link href="https://github.com/kevinAcevedo240/make-it-grid-ai" target="_blank">
-            <Icons.GitHub className="size-5 text-white" />
-            <span className="sr-only">GitHub</span>
-          </Link>
+        <Button
+          onClick={handleClick}
+          className="github-step dark:border-primary bg-muted-foreground dark:border dark:bg-transparent active:scale-90"
+        >
+          <Icons.GitHub className="size-5 text-white" />
+          <span className="sr-only">GitHub</span>
         </Button>
       </TooltipTrigger>
-      <TooltipContent  className="md:block hidden text-center">It&apos;s Open Source! <br/> If you like it, consider giving it a star! 🌟</TooltipContent>
+      <TooltipContent className="md:block hidden text-center">
+        It&apos;s Open Source! <br /> If you like it, consider giving it a star!
+        🌟
+      </TooltipContent>
     </Tooltip>
   );
 };
--- components/menu/web.tsx
+++ components/menu/web.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 import * as Icons from "@/components/icons";
 import { Button } from "../ui/button";
 import {
@@ -9,17 +7,24 @@ import {
 } from "@/components/ui/tooltip";
 
 export const Website = () => {
+  const handleClick = () => {
+    window.open("https://www.kevin-acevedo.dev/", "_blank");
+  };
+
   return (
     <Tooltip>
       <TooltipTrigger asChild>
-        <Button className="website-step border-primary border dark:bg-primary/0 active:scale-90">
-          <Link href="https://www.kevin-acevedo.dev/" target="_blank">
-            <Icons.Globe className="size-5" />
-            <span className="sr-only">Website</span>
-          </Link>
+        <Button 
+          className="website-step border-primary border dark:bg-primary/0 active:scale-90"
+          onClick={handleClick}
+        >
+          <Icons.Globe className="size-5" />
+          <span className="sr-only">Website</span>
         </Button>
       </TooltipTrigger>
-      <TooltipContent  className="md:block hidden">Visit my Website</TooltipContent>
+      <TooltipContent className="md:block hidden">
+        Visit my Website
+      </TooltipContent>
     </Tooltip>
   );
 };
