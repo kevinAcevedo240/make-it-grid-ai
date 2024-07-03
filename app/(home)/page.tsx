775f54fb16e8772acdd2fc7cb980c598edd11c2f
Kevin Julian Acevedo Salazar <kevinAcevedo240@users.noreply.github.com>
2024-07-02 22:01:54 -0500

    Remove: Particles component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,22 +4,10 @@
 import GridLayout from "@/components/GridLayout";
 import { TourProvider } from '@reactour/tour'
 import { steps, styles } from "@/utils/guide-steps";
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
-import Particles from "@/components/ui/particles";
 import NavBar from "@/components/navbar";
 import { FloatingMenu } from "@/components/menu/floating-menu";
 
-
-
 export default function Home() {
-  const { theme } = useTheme();
-  const [color, setColor] = useState("#ffffff");
- 
-  useEffect(() => {
-    setColor(theme === "dark" ? "#ffffff" : "#000000");
-  }, [theme]);
-  
   return (
     <TourProvider
       className="rounded-lg !bg-muted"
@@ -30,14 +18,6 @@ export default function Home() {
       <NavBar/>
       <GridLayout/>
       <FloatingMenu />
-      <Particles
-        className="absolute inset-0 opacity-50"
-        quantity={100}
-        ease={60}
-        color={color}
-        refresh
-      />
-
     </TourProvider>
   );
 }
