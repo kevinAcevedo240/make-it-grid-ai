a5a5a346daeb90dff11a19292787aa224368508f
Kevin Julian Acevedo Salazar <kevinAcevedo240@users.noreply.github.com>
2024-07-02 19:15:59 -0500

    Add: Aria Values to desktop and mobile toggle for  Accesbility issues

--- components/menu/responsive-toggle.tsx
+++ components/menu/responsive-toggle.tsx
@@ -1,32 +1,45 @@
-
-
-
 import * as Icons from "@/components/icons";
 import {
     Tooltip,
     TooltipContent,
     TooltipTrigger,
-  } from "@/components/ui/tooltip";
-import { Tabs,  TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { useContext } from "react";
+} from "@/components/ui/tooltip";
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { useContext, useState } from "react";
 import { GridContext } from "@/context/useGridContext";
 
 export const ResponsiveToggle = () => {
+  const { setIsMobile } = useContext(GridContext);
+  const [selectedTab, setSelectedTab] = useState("desktop");
 
-  const { setIsMobile} = useContext(GridContext);
+  const handleTabChange = (value: string) => {
+    setSelectedTab(value);
+    setIsMobile(value === "mobile");
+  };
+
   return (
     <Tooltip>
       <TooltipTrigger asChild>
         <Tabs
-        onValueChange={(value) => setIsMobile(value === "mobile")}
+          onValueChange={handleTabChange}
           defaultValue="desktop"
           className="responsive-step overflow-x-auto rounded-lg"
         >
           <TabsList className="bg-muted-foreground/40 dark:bg-muted">
-            <TabsTrigger value="mobile" aria-label="Mobile Mode">
+            <TabsTrigger
+              value="mobile"
+              aria-label="Mobile Mode"
+              aria-controls="mobile-tab-content"
+              aria-selected={selectedTab === "mobile"}
+            >
               <Icons.Mobile className="size-5 text-white" />
             </TabsTrigger>
-            <TabsTrigger value="desktop" aria-label="Desktop Mode">
+            <TabsTrigger
+              value="desktop"
+              aria-label="Desktop Mode"
+              aria-controls="desktop-tab-content"
+              aria-selected={selectedTab === "desktop"}
+            >
               <Icons.Desktop className="size-5 text-white" />
             </TabsTrigger>
           </TabsList>
