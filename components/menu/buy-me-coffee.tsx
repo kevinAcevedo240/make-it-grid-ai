f1b0c6eebbc1cc30e32f39fd59c9baec7ce665b7
Kevin Julian Acevedo Salazar <kevinAcevedo240@users.noreply.github.com>
2024-07-08 09:28:47 -0500

    Fix: Auto size in buymecoffee.png

--- components/menu/buy-me-coffee.tsx
+++ components/menu/buy-me-coffee.tsx
@@ -22,7 +22,7 @@ const BuyMeCoffee: React.FC = () => {
             className="buymecoffee-step gap-2 border-primary border dark:bg-primary/0 active:scale-90"
           >
             <BuyMeCoffeBrand className='size-5'/>
-            <Image className="hidden md:block text-white" src="/buymecoffee.png" alt="Buy me a coffee" width={110} height={110} />
+            <Image className="hidden md:block text-white w-auto h-auto" src="/buymecoffee.png" alt="Buy me a coffee" width={100} height={100} />
           </Button>
         </TooltipTrigger>
         <TooltipContent className="md:block hidden text-center">
